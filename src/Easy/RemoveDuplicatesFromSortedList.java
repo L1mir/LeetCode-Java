644376bb25f39776f494f5cbe299bf0af6dbb2a7
L1mir <vladimir.puchynski@gmail.com>
2024-04-09 02:47:21 +0300

    TreeNode

---
+++ src/Easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode sHead = new ListNode(), sTail = sHead, node = head;
+
+        while (node != null) {
+            while (node.next != null && node.val == node.next.val) {
+                node = node.next;
+            }
+
+            ListNode next = node.next;
+            node.next = null;
+            sTail.next = node;
+            sTail = node;
+            node = next;
+        }
+
+        return sHead.next;
+    }
+}
