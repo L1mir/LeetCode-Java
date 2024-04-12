3b25e205c406a18b604865f5f98afc17a0a9c15b
L1mir <vladimir.puchynski@gmail.com>
2024-04-13 00:59:13 +0300

    MinimumDepthOfBinaryTree

--- src/Hard/MedianOfTwoSortedArrays.java
+++ src/Hard/MedianOfTwoSortedArrays.java
@@ -1,5 +1,7 @@
 package Hard;
 
 public class MedianOfTwoSortedArrays {
-
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return (nums1.length + nums2.length) / 2.0;
+    }
 }
