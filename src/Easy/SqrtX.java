0e0ae101d2cb8b430cd609754eb54a9a3dab2dca
L1mir <vladimir.puchynski@gmail.com>
2024-04-15 02:45:18 +0300

    new commit

--- src/Easy/SqrtX.java
+++ src/Easy/SqrtX.java
@@ -1,5 +1,20 @@
 package Easy;
 
 public class SqrtX {
+    public int mySqrt(int x) {
+        long high = x;
+        long low = 0;
 
+        while(low <= high){
+            long mid = (high + low) / 2;
+            if((mid * mid) == x) {
+                return (int)mid;
+            }
+            else if ((mid * mid) > x) {
+                high = mid-1;
+            }
+            else low = mid + 1;
+        }
+        return (int)high;
+    }
 }
