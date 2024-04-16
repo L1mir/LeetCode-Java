246cf98e87bca28f6e0f9c0706f715c03a4afd07
L1mir <vladimir.puchynski@gmail.com>
2024-04-17 01:11:33 +0300

    new commit

--- src/Easy/HowManyNumbersAreSmallerThanTheCurrentNumber.java
+++ src/Easy/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -1,23 +1,4 @@
 package Easy;
 
 public class HowManyNumbersAreSmallerThanTheCurrentNumber {
-    public int[] smallerNumbersThanCurrent(int[] nums) {
-        int lengthNums = nums.length;
-        int[] result = new int[lengthNums];
-        for(int i = 0; i < lengthNums; i++){
-            int currentNum = nums[i];
-            int count = 0;
-            for(int j= 0; j < lengthNums; j++){
-                if(i == j){
-                    continue;
-                }else{
-                    if(nums[j] < nums[i]){
-                        count++;
-                    }
-                }
-            }
-            result[i] = count;
-        }
-        return result;
-    }
 }
