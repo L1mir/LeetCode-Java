baa274ccd298b9cda9d5bc961024fafb6094e68b
Limir <vladimir.puchynski@gmail.com>
2024-08-31 00:18:41 +0300

    new

--- src/Easy/Bit12Character.java
+++ src/Easy/Bit12Character.java
@@ -7,7 +7,7 @@ public class Bit12Character {
         while (i < bits.length - 1) {
             i = (bits[i] == 1) ? i + 2 : i + 1;
         }
-
+asffasfsafsfas
         return i == bits.length - 1;
     }
 }
